03ed3b93e0b62c1b472fac004715e4beea10c69e
buingoclam147 <buingoclam00@gmail.com>
2024-06-15 23:36:57 +0700

    chore: Update maximumWarning and maximumError values for anyComponentStyle

--- src/app/modules/pokemon-list/pokemon-detail/pokemon-detail.component.ts
+++ src/app/modules/pokemon-list/pokemon-detail/pokemon-detail.component.ts
@@ -1,49 +1,52 @@
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { Component, inject, Input, signal } from '@angular/core';
-import { exhaustMap, filter, finalize, of, startWith, tap } from 'rxjs';
-import { IDetailPokemonDto } from '../../../core/models/pokemon.model';
-import { PokemonService } from '../../../core/services/pokemon.service';
-
+import { ChangeDetectionStrategy, Component, inject, Input, signal, ViewEncapsulation } from '@angular/core';
+import { IDetailPokemonDto } from '@core/models/pokemon.model';
+import { PokemonService } from '@core/services/pokemon.service';
+import { SpinService } from '@core/services/spin.service';
+import { delay, exhaustMap, filter, of, startWith, tap } from 'rxjs';
+const DATA_DEFAULT = {
+  data: {
+    id: '',
+    number: 0,
+    name: '',
+    type_1: 0,
+    type_2: null,
+    total: 0,
+    hp: 0,
+    attack: 0,
+    defense: 0,
+    sp_atk: 0,
+    sp_def: 0,
+    speed: 0,
+    generation: 0,
+    legendary: 0,
+    created_at: '',
+    updated_at: ''
+  }, status: 0, success: false
+}
 @Component({
   selector: 'pokemon-detail[id]',
   standalone: true,
   imports: [CommonModule, NgOptimizedImage],
   templateUrl: './pokemon-detail.component.html',
-  styleUrl: './pokemon-detail.component.scss'
+  styleUrl: './pokemon-detail.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  encapsulation: ViewEncapsulation.None
 })
 export class PokemonDetailComponent {
-  private readonly pokemonSvc = inject(PokemonService)
+  private readonly pokemonSvc = inject(PokemonService);
+  private readonly ss = inject(SpinService);
   @Input({ required: true }) id!: string;
   dataPokemon = signal<IDetailPokemonDto | null>(null);
-  isLoading = signal(true);
   readonly pokemons$ =
     of(null).pipe(
       filter(() => !!this.id),
       exhaustMap(_ => this.pokemonSvc.getPokemonById$(this.id)
         .pipe(
-          // delay(1000),
+          delay(1000),
           tap((response) => this.dataPokemon.set(response)),
-          finalize(() => this.isLoading.set(false)),
-          startWith({
-            data: {
-              id: '',
-              number: 0,
-              name: '',
-              type_1: 0,
-              type_2: null,
-              total: 0,
-              hp: 0,
-              attack: 0,
-              defense: 0,
-              sp_atk: 0,
-              sp_def: 0,
-              speed: 0,
-              generation: 0,
-              legendary: 0,
-              created_at: '',
-              updated_at: ''
-            }, status: 0, success: false
-          })
+          this.ss.spinningPageWithObs$(),
+          startWith(DATA_DEFAULT)
         ))
     )
 }
